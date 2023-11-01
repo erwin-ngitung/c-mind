f842b715981e0c204556bca8c1d322a9f36e24c0
Erwin Fernanda <fernandaerwin277@gmail.com>
2023-11-01 23:44:17 +0700

    Update app

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -22,13 +22,13 @@ st1, st2, st3 = st.columns(3)
 with st2:
     st.image(image)
 
-st.markdown('<h3 style=\'text-align:center;\'>Welcome to SMD 1.0! 👋</h3>', unsafe_allow_html=True)
+st.markdown('<h3 style=\'text-align:center;\'>Welcome to C-MIND 1.0! 👋</h3>', unsafe_allow_html=True)
 
 st.markdown(
     """
     ### Want to learn more and purchase it?
-    - Check out [SMD 1.0 Website] (https://github.com/erwin-ngitung/smd-1.0)
-    - Jump into our [Documentation] (https://github.com/erwin-ngitung/smd-1.0)
-    - Ask a question in our [Company] (corporate.secretary@medcoenergi.com)
+    - Check out [C-MIND 1.0 Website] (https://github.com/erwin-ngitung/c-mind-1.0)
+    - Jump into our [Documentation] (https://github.com/erwin-ngitung/c-mind-1.0)
+    - Ask a question in our software (salsabilsudharta0724@gmail.com)
     """
 )
